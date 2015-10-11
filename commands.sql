2f55cd70dd0f073ea55909e84ab4bb030f832cb2
candyapplecorn <candyapplecorn@gmail.com>
2015-10-11 02:53:20 -0700

    modified related tables and added an update hospital procedure

--- commands.sql
+++ commands.sql
@@ -14,6 +14,9 @@ fuel INT DEFAULT 0 CHECK (FUEL >= 0),
 alliance VARCHAR(255) DEFAULT '',       -- For optional alliances (teams)
 rank INT DEFAULT 0,                     -- If we decide to add a level-up / rank system
 alerts INT DEFAULT 0, 
+hospital_level INT DEFAULT 0,
+attack_level INT DEFAULT 0,
+defense_level INT DEFAULT 0,
 /* rowsowned VARCHAR(255) DEFAULT '',*/ -- Not needed in all likelihood.
 lastlogin DATETIME 
 );
@@ -24,7 +27,7 @@ The whole game recolves around rows, so it should be a pretty detailed table!
 CREATE TABLE IF NOT EXISTS gamerows ( 
 id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, 
 owner INT DEFAULT 0 REFERENCES players(id),
-ownerusername VARCHAR(255) DEFAULT '', -- select username from players join gamerows as g where id = g.id;
+ownerusername VARCHAR(255) DEFAULT '', 
 morale INT DEFAULT 0,
 defenders INT DEFAULT 0 CHECK (defenders >= 0),
 attackers INT DEFAULT 0 CHECK (attackers >= 0),
@@ -33,6 +36,7 @@ fuel INT DEFAULT 0 CHECK (FUEL >= 0),
 mgs INT DEFAULT 0 CHECK (MGS >= 0),
 fgs INT DEFAULT 0 CHECK (FGS >= 0),
 dgs INT DEFAULT 0 CHECK (DGS >= 0),
+hospital INT DEFAULT 0 CHECK (hospital >= 0),
 investments INT DEFAULT 0 CHECK (investments >= 0),
 investors VARCHAR(255) DEFAULT '', -- This will be a string containing a JSON object.
 lastattacked DATETIME,
@@ -61,7 +65,7 @@ BEGIN
 	UPDATE gamerows
 	SET owner=user_id, 
 	ownerusername=(SELECT username FROM players WHERE id=user_id),
-	morale=DEFAULT(morale), defenders=DEFAULT(defenders), attackers=DEFAULT(attackers),
+	morale=100, defenders=DEFAULT(defenders), attackers=DEFAULT(attackers),
 	money=DEFAULT(money), fuel=DEFAULT(fuel), mgs=DEFAULT(mgs), fgs=DEFAULT(fgs),
 	dgs=DEFAULT(dgs), investments=DEFAULT(investments), investors='',
 	lastaccessed=NOW()
@@ -112,6 +116,27 @@ END upd_r_access;
 //
 delimiter ;
 
+-- updates a given row's hospital column
+-- drop procedure upd_r_hospital;
+delimiter //
+CREATE procedure upd_r_hospital(row_id int)
+this_proc: BEGIN
+DECLARE period, chng, hsptl INT DEFAULT 0;
+SET hsptl = (select hospital from gamerows where id = row_id);
+IF hsptl <= 0 THEN
+LEAVE this_proc;
+END IF;
+SET period = (select timestampdiff(second, (select lastaccessed from gamerows where id = row_id), now()));
+SET chng = (select period * (1 + (select hospital_level from players where players.id = row_id)));
+SET chng = (select IF(chng > hsptl, hsptl, chng));
+UPDATE gamerows
+SET defenders = defenders + chng,
+hospital = hospital - chng
+WHERE id = row_id;
+END this_proc;
+//
+delimiter ;
+
 -- procedure to update a row
 -- drop procedure upd_all;
 delimiter //
@@ -144,8 +169,8 @@ Now that we've entered our two main tables,
 let's populate them with some values.
 */
 
--- Let's make 100 rows in gamerows. 
-CALL fillgamerows(100);
+-- Let's make 50 rows in gamerows. 
+CALL fillgamerows(50);
 
 -- Now, we'll insert three players.
 INSERT INTO players (username, password, lastlogin)
@@ -171,3 +196,7 @@ CALL grantrow(12, 4);
 CALL grantrow(12, 2);
 CALL grantrow(11, 2);
 CALL grantrow(2, 2);
+
+UPDATE gamerows
+SET hospital = 1000
+WHERE id = 2;
