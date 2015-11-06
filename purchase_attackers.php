ae7f360ce2ad7aaf8b026d01f481b9796f428e03
candyapplecorn <candyapplecorn@gmail.com>
2015-11-06 15:38:02 -0800

    updated purchase_attackers

--- purchase_attackers.php
+++ purchase_attackers.php
@@ -1,38 +1,27 @@
+<?PHP include "scripts/headers.php"; ?>
+
 <form action="" method="post">
 row: <input type="text" name="row"> amt: <input type="text" name="amt">
 <input type="submit">
 </form>
+
 <?PHP
-include "scripts/headers.php";
-	$row=0;
-	$amt=0;
-	if(isset($_POST['row'])&&ctype_digit($_POST['row'])&&($_POST['amt'])&&ctype_digit($_POST['amt'])){
-		$row=$_POST['row'];
-		$amt=$_POST['amt'];
+if(isset($_POST['row'])&&ctype_digit($_POST['row'])&&($_POST['amt'])&&ctype_digit($_POST['amt'])){
+	$row=$_POST['row'];
+	$amt=$_POST['amt'];
+
+	$conn = new Connection();
+
+	$qarr = Array(':name'=>$_SESSION["name"], ':id'=>$row);
+	$qstr = "Select id from gamerows where ownerusername = :name AND id = :id";
+	$results = $conn->Custom_Query($qstr, $qarr, TRUE);
+
+	$qarr = Array('row'=>$row,'amt'=>$amt);
+	$qstr = "CALL purchase_attackers(:row, :amt)";
+
+	if(isset($results[0]["id"])){
+		$conn->Custom_Query($qstr, $qarr, TRUE);
 	}
-	
-$conn = new Connection();
-
-$qarr = Array(':name'=>$_SESSION["name"]);
-$qstr = "Select id from gamerows where ownerusername = :name";
-$results = $conn->Custom_Query($qstr, $qarr, TRUE);
-
-$flag = FALSE;
-
-foreach ($results AS $inner){
-	foreach ($inner as $value){
-		if($row==$value){
-			$flag=TRUE;
-		
-		}
-	}
-}
-
-$qarr = Array('row'=>$row,'amt'=>$amt);
-$qstr = "CALL purchase_attackers(:row, :amt)";
-
-if($flag){
-	$conn->Custom_Query($qstr, $qarr, TRUE);
 }
 include "map.php";
 include "ownedrows.php";
