7bd5e34076254a79873224cd72b1111508a3ac20
ammc <alexander.hdwctw@gmail.com>
2015-11-10 14:03:53 -0800

    add 1/2 the buttons for proper game play

---
+++ pub/js/script.js
@@ -0,0 +1,10 @@
+$("#login").click(function() {
+	$("#login-box").toggle('slow');
+	$("#signup-box").hide('slow');
+});
+$("#signup").click(function() {
+	$("#signup-box").toggle('slow');
+	$("#login-box").hide('slow');
+});
+
+	
